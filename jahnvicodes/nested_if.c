fc28a2c0482719992855c48e2a3922b901c40b92
the-inker-imp <91970777+the-inker-imp@users.noreply.github.com>
2021-10-05 21:39:48 +0530

    Create nested_if.c

---
+++ jahnvicodes/nested_if.c
@@ -0,0 +1,20 @@
+//This program is of nested if in C, to find the grestest number among the three given numbers//
+
+
+#include<stdio.h>
+int main()
+{
+    int n1=54 , n2=55 , n3=56;
+
+if(n2>n1) 
+{
+    if(n3>n1)
+    {
+        if(n3>n1)
+        {
+            printf("n3 is the largest no.");
+        }
+    }
+}
+    return 0;
+}
